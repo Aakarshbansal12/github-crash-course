893c8847f34f774bc369a5efec6816927628d49c
Aakarsh <aakarshbansal888@gmail.com>
2024-03-03 17:30:43 +0530

    Added isabove18 fn

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const age=20;
-const name="Aakarsh";
+const name1="Aakarsh";
 const date="3 march";
+
+function sayname(){
+    console.log('My name is'+name1);
+}
+
+function isabove18(){
+    return age>=18;
+}
