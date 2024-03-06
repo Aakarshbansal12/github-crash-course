629536eb66c8373ced0ce68537ad36ea5e41c5c7
Aakarsh <aakarshbansal888@gmail.com>
2024-03-06 22:54:29 +0530

    revert back

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ function sayname(){
     console.log('My name is'+name1);
 }
 
+<<<<<<< HEAD
 function newfn(){
     console.log('Hey i am a new function');
+=======
+function isabove18(){
+    return age>=18;
+>>>>>>> parent of 9ec37f6 (remove 18 fn)
 }
